c2901506af6680cdb9113ffb7ebb59b0bfd9892f
Ravindranath K <ravindranath.k@gmail.com>
2024-11-24 15:53:20 +0530

    updated alias for webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "src/pages/Home";
+import About from "src/pages/About";
+import NotFound from "src/pages/NotFound";
 
 const App = () => {
   return (
     <div>
-      <h1>React SPA with Webpack!</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,7 +9,11 @@ module.exports = {
     publicPath: "/",
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js", ".jsx", ".json"],
+    alias: {
+      // Add alias for 'src' to simplify imports
+      src: path.resolve(__dirname, "../src/"), // Alias 'src' to the actual src folder
+    },
   },
   module: {
     rules: [
